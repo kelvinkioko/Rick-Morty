7de0326c278d73636e012a7db104bffd63326e4c
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-12-25 16:18:04 +0300

    Hilt added

---
+++ app/src/main/java/graph/rick/morty/app/RickApp.kt
@@ -0,0 +1,7 @@
+package graph.rick.morty.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickApp : Application()
--- app/src/main/java/graph/rick/morty/presentation/MainActivity.kt
+++ app/src/main/java/graph/rick/morty/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package graph.rick.morty
+package graph.rick.morty.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import graph.rick.morty.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
